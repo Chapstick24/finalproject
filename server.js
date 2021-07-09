9429cdca43bcce5feca4f80b14b3f874fbbd9c6d
mychal <mychal9251@gmail.com>
2021-07-08 21:31:30 -0700

    server stuff

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 //"mongodb://localhost/greenstream"
 
 // Add routes, both API and view
